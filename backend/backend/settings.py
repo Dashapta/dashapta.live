c12190b6d8dbbd5bb9b51640f86e99d53008311a
Bindu Kiran <dashaptatrust@gmail.com>
2025-12-07 17:03:18 +0530

    Fix: removed non-existent 'authentication' app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication',
     'rest_framework',
     'corsheaders',
     'scholarship',
